ab5440241fbc7512871f87f202520510b323a1da
fabricastro <fabricastrook@gmail.com>
2024-10-29 21:50:57 -0300

    cambios en modal fix errors

--- src/hoc/withWelcomeModal.tsx
+++ src/hoc/withWelcomeModal.tsx
@@ -17,10 +17,10 @@ function withWelcomeModal<T>(WrappedComponent: React.ComponentType<T>) {
             const lastShown = localStorage.getItem('lastWelcomeModalShown');
             const now = Date.now();
 
-            console.log("Business info complete:", businessInfoComplete);
-            console.log("Last shown time:", lastShown);
-            console.log("Current time:", now);
-            console.log("Time difference:", lastShown ? now - parseInt(lastShown, 10) : "No last shown time");
+            // console.log("Business info complete:", businessInfoComplete);
+            // console.log("Last shown time:", lastShown);
+            // console.log("Current time:", now);
+            // console.log("Time difference:", lastShown ? now - parseInt(lastShown, 10) : "No last shown time");
 
             if (!businessInfoComplete && (!lastShown || now - parseInt(lastShown, 10) > MODAL_INTERVAL_MS)) {
                 setShowWelcomeModal(true);
